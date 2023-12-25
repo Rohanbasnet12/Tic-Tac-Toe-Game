31559d83f8cdbb6861409f8acbbaaf9f7c228d14
Rohanbasnet12 <rohanbmyname@gmail.com>
2023-12-25 08:29:58 -0800

    Second commit

--- app.js
+++ app.js
@@ -7,30 +7,53 @@ let player2 = 'O';
 const attempt = [];
 $('#player').html(player1); // Initialize with player1
 
-// Example with a jQuery collection
+
 $('.box').each(function () {
     $(this).on('click', () => {
-        if (player1Turn === true) { // If player1 Turn
-            $(this).text(player1); // Inserting X on the clicked container
+        if (!$(this).text()) { // Check if the box is empty
+            if (player1Turn === true) { // If player1 Turn
+                $(this).text(player1); // Inserting X on the clicked container
 
-            attempt.push(player1);
-            player1Turn = false;
-            $('#player').html(player2); // Update player indicator to player2
-        } else { // If player2 Turn
-            $(this).text(player2); // Inserting O on the clicked container
+                attempt.push(player1);
+                player1Turn = false;
+                $('#player').html(player2); // Update player indicator to player2
+            } else { // If player2 Turn
+                $(this).text(player2); // Inserting O on the clicked container
 
-            attempt.push(player2);
-            player1Turn = true;
-            $('#player').html(player1); // Update player indicator to player1
+                attempt.push(player2);
+                player1Turn = true;
+                $('#player').html(player1); // Update player indicator to player1
+            }
+
+            console.log(attempt);
+            if (attempt.length === 9) {
+                console.log('Game over');
+                $('#display').html('GAME OVER !');
+                $('.restart').css({
+                    'display': 'block',
+                });
+            }
         }
-
-        console.log(attempt);
-        if (attempt.length === 9) {
-            console.log('Game over');
-            $('#display').html('GAME OVER !');
-            $('.restart').css({
-                'display': 'block',
-            });
-        }
+        forXToWin();
+        forYToWin();
     })
 });
+
+
+function forXToWin() {
+    if (attempt[0] == 'X' && attempt[2] == 'X' && attempt[4] == 'X') {
+        $('#display').html(`Player ${player1} Won`);
+        $('.restart').css({
+            'display': 'block',
+        });
+    }
+}
+
+function forYToWin() {
+    if (attempt[0] == 'O' && attempt[2] == 'O' && attempt[4] == 'O') {
+        $('#display').html(`Player ${player2} Won`);
+        $('.restart').css({
+            'display': 'block',
+        });
+    }
+}
