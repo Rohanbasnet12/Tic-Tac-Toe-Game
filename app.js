cd88d4f81226cd4d861f283e200f998b80581ad1
Rohanbasnet12 <rohanbmyname@gmail.com>
2023-12-26 23:20:09 -0800

    Third commit

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ let player1Turn = true;
 
 let player2 = 'O';
 
-// CREATING AN EMPTY ARRAY FOR USER ATTEMPTS 
-const attempt = [];
+const attempt = []; // CREATING AN EMPTY ARRAY FOR USER ATTEMPTS 
 $('#player').html(player1); // Initialize with player1
 
+// DEFINING DIFFERENT AUDIO FOR IMMERSIVE EXPERIENCE
+
+let xTurnAudio = new Audio('./sounds/blue.mp3');
+let yTurnAudio = new Audio('./sounds/green.mp3');
+let gameOverAudio = new Audio('./sounds/wrong.mp3');
 
 $('.box').each(function () {
     $(this).on('click', () => {
         if (!$(this).text()) { // Check if the box is empty
             if (player1Turn === true) { // If player1 Turn
                 $(this).text(player1); // Inserting X on the clicked container
-
+                xTurnAudio.play();
                 attempt.push(player1);
                 player1Turn = false;
                 $('#player').html(player2); // Update player indicator to player2
             } else { // If player2 Turn
                 $(this).text(player2); // Inserting O on the clicked container
-
+                yTurnAudio.play();
                 attempt.push(player2);
                 player1Turn = true;
                 $('#player').html(player1); // Update player indicator to player1
@@ -27,6 +31,7 @@ $('.box').each(function () {
 
             console.log(attempt);
             if (attempt.length === 9) {
+                gameOverAudio.play();
                 console.log('Game over');
                 $('#display').html('GAME OVER !');
                 $('.restart').css({
@@ -34,26 +39,6 @@ $('.box').each(function () {
                 });
             }
         }
-        forXToWin();
-        forYToWin();
+
     })
 });
-
-
-function forXToWin() {
-    if (attempt[0] == 'X' && attempt[2] == 'X' && attempt[4] == 'X') {
-        $('#display').html(`Player ${player1} Won`);
-        $('.restart').css({
-            'display': 'block',
-        });
-    }
-}
-
-function forYToWin() {
-    if (attempt[0] == 'O' && attempt[2] == 'O' && attempt[4] == 'O') {
-        $('#display').html(`Player ${player2} Won`);
-        $('.restart').css({
-            'display': 'block',
-        });
-    }
-}
